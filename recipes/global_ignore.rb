d468fae5016c475e217b96f6099b4d6e80ffc4b6
Brian Jennings <brian.j.jennings@gmail.com>
2014-08-04 23:58:21 -0700

    Modifying cookbooks for system ruby 1.8.X

--- recipes/global_ignore.rb
+++ recipes/global_ignore.rb
@@ -1,5 +1,5 @@
 template "#{node['sprout']['home']}/.gitignore_global" do
   source 'gitignore_global.erb'
   owner node['current_user']
-  variables(ignore_idea: node[:git_global_ignore_idea])
+  variables(:ignore_idea => node[:git_global_ignore_idea])
 end
